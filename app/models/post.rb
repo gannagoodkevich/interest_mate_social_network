083ce5d18547aebd9aad1430d303d7a05479e3ac
gannagoodkevich <gannagoodkevich@gmail.com>
2020-01-28 15:41:55 +0300

    Add many_to_many relation between Post and Category

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ApplicationRecord
   has_many :post_tags
   has_many :tags, through: :post_tags
   has_many :comments
+  has_many :post_categories
+  has_many :categories, through: :post_categories
   has_one :user
 end
