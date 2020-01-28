039098d4741845bef640cdf693346c4c462322c2
gannagoodkevich <gannagoodkevich@gmail.com>
2020-01-28 18:20:31 +0300

    Make #create, #show, #index methods & views for Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,28 @@
 class PostsController < ApplicationController
+  def new
+    @user = User.find_by(id: params[:user_id])
+  end
+
+  def create
+    @user = User.find_by(id: params[:user_id])
+    @post = @user.posts.create!(post_params)
+    redirect_to user_posts_path
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:user_id])
+    puts @user.inspect
+    @post = @user.posts.find_by(id: params[:id])
+    puts @post.inspect
+  end
+
+  private
+
+  def post_params
+    params.permit(:title, :content)
+  end
 end
