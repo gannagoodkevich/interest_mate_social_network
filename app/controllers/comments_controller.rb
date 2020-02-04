87bc4c2f79cd6b5b3679bfc7908d2968ecfd5062
gannagoodkevich <gannagoodkevich@gmail.com>
2020-02-04 16:47:21 +0300

    Create commentable polymorphic relation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,16 @@
 class CommentsController < ApplicationController
+  before_action :find_commentable
   def index
-    @post = Post.find_by(id: params[:post_id])
     @user = @post.user
-    @comments = @post.comments
+    @comments = @commentable.comments
     @comment = Comment.new
   end
 
   def create
     @post = Post.find_by(id: params[:post_id])
-    @user = User.first
-    @comment = @user.comments.create!(comment_params)
-    @post.comments << @comment
+    @user = User.last
+    @comment = @commentable.comments.create!(comment_params)
+    @user.comments << @comment
     redirect_to user_post_comments_path
   end
 
@@ -18,5 +18,10 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def find_commentable
+    @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
+    @commentable = Post.find_by_id(params[:post_id]) if params[:post_id]
   end
 end
