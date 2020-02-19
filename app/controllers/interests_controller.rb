0354ac3c5b224458528cd0d9c44d9f3926ae141b
gannagoodkevich <gannagoodkevich@gmail.com>
2020-02-19 13:11:54 +0300

    Refactor user information input. Add interest choosing page with ajax

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,5 +1,12 @@
 class InterestsController < ApplicationController
   def create
-
+    @user = User.find_by(id: params[:user_id])
+    params[:interests].each do |_key, value|
+      puts value
+      @user.interests << Interest.find_by(name: value)
+    end
+    respond_to do |format|
+      format.js
+    end
   end
 end
