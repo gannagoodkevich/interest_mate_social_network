2904fd8d8f04f49457977f80da34a4ed01ed81e5
gannagoodkevich <gannagoodkevich@gmail.com>
2020-02-19 15:47:19 +0300

    Resolve rubocop offences

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -8,13 +8,8 @@ class InterestsController < ApplicationController
   def update
     @user = User.find_by(id: params[:user_id])
     @interest_categories = InterestCategory.all
-    @user.user_interests.each { |user_interest| user_interest.delete }
-    params[:interests].each do |_key, value|
-      puts value
-      value.each do |interest|
-        @user.interests << Interest.find_by(name: interest)
-      end
-    end
+    @user.user_interests.each(&:delete)
+    user_interest_update
     respond_to do |format|
       format.js
     end
@@ -30,14 +25,20 @@ class InterestsController < ApplicationController
 
   def create
     @user = User.find_by(id: params[:user_id])
+    user_interest_update
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+  def user_interest_update
     params[:interests].each do |_key, value|
       puts value
       value.each do |interest|
         @user.interests << Interest.find_by(name: interest)
       end
-    end
-    respond_to do |format|
-      format.js
     end
   end
 end
